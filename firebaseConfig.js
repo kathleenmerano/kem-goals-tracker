a968dc778a21744b18f474787ed017436cb82507
kathleenmerano <kathleenmerano3@gmail.com>
2025-08-28 11:42:42 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBupRb0Csv9hv-19uESJmf8i0zY9E8McTM",
+  authDomain: "kem-goal-tracker.firebaseapp.com",
+  projectId: "kem-goal-tracker",
+  storageBucket: "kem-goal-tracker.firebasestorage.app",
+  messagingSenderId: "803582149267",
+  appId: "1:803582149267:web:7af71ec90e3b65e0d10366",
+};
+
+const app = initializeApp(firebaseConfig);
