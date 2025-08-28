42507079daabe566b4d6fe5e1bbc50476ab4253e
kathleenmerano <kathleenmerano3@gmail.com>
2025-08-28 10:11:57 +0800

    color for buttons

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#737dd6ff',
     color: 'white',
     borderRadius: 8,
   }
--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#737dd6ff',
     color: 'white',
     borderRadius: 8,
   },
